b892879248f821473edcd237a34d7cd4c8b066d5
junha-ahn <junha.ahn.dev@gmail.com>
2020-03-30 16:15:01 +0900

    add createNewTransaction method

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -9,22 +9,36 @@ class Block {
   ) {
   }
 }
+class Transaction {
+  constructor(
+    public amount:number,
+    public sender: string,
+    public recipient: string,
+  ) {
+  }
+}
 
 export default class BlockChain {
   static chain: Block[] = []
-  static newTransactions = []
+  static pendingTransactions: Transaction[] = []
   constructor() {}
   static createNewBlcok = (nonce, previousBlockHash, hash) => {
     const newBlock = new Block(
       BlockChain.chain.length + 1,
       nonce,
-      BlockChain.newTransactions,
+      BlockChain.pendingTransactions,
       hash,
       previousBlockHash,
       Date.now()
     )
-    // BlockChain.newTransactions = [];
+    BlockChain.pendingTransactions = [];
     BlockChain.chain.push(newBlock)
     return newBlock
+  }
+  static getLastBlcok = () => BlockChain.chain[BlockChain.chain.length - 1]
+  static createNewTransaction = (amount: number, sender: string, recipient: string) => {
+    const newTransaction = new Transaction(amount, sender, recipient)
+    BlockChain.pendingTransactions.push(newTransaction)
+    return BlockChain.getLastBlcok().index + 1
   }
 }
