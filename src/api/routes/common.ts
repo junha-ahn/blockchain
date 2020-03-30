6312afda52803e50af733e0cab3379861e6135d8
junha-ahn <junha.ahn.dev@gmail.com>
2020-03-31 00:23:47 +0900

    add route (blockchain, transaction, mine)

--- src/api/routes/common.ts
+++ src/api/routes/common.ts
@@ -15,10 +15,43 @@ export default (app: Router) => {
     }
   }))
 
-  route.get('/bloc', container(async (req): Promise<Result> => {
+  route.get('/blockchain', container(async (req): Promise<Result> => {
     return {
       httpCode: 200,
-      message: 'hello World'
+      data: bitconin,
+    }
+  }))
+
+  route.post('/transaction', container(async (req): Promise<Result> => {
+    const {
+      amount,
+      sender,
+      recipient,
+    } = req.body
+    const blockIndex: number = bitconin.createNewTransaction(amount, sender, recipient)
+    return {
+      httpCode: 200,
+      message: `Transaction will be added in block ${blockIndex}`,
+      data: blockIndex,
+    }
+  }))
+
+  route.get('/mine', container(async (req): Promise<Result> => {
+    const lastBlock = bitconin.getLastBlcok()
+    const previousBlockHash = lastBlock.hash
+    const currentBlockData = {
+      transactions: bitconin.pendingTransactions,
+      index: lastBlock.index + 1
+    }
+    const nonce = bitconin.proofOfWerk(previousBlockHash, currentBlockData)
+    const blockHash = bitconin.hashBlock(previousBlockHash, currentBlockData, nonce)
+    const newBlock = bitconin.createNewBlcok(nonce, previousBlockHash, blockHash)
+
+    // bitconin.createNewTransaction(12.5, "00", nodeAddress) 
+    // TODO: get node address
+    return {
+      httpCode: 200,
+      data: newBlock,
     }
   }))
 }
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,10 +1,14 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import routes from '../api';
-import config from '../config';
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as morgan from 'morgan'
+import routes from '../api'
+import config from '../config'
 
 export default ({ app }: { app: express.Application }) => {
-  app.use(bodyParser.json());
+  
+  app.use(morgan(config.NODE_ENV == "production" ? "combined" : "dev"));
+
+  app.use(bodyParser.json())
   // Load API routes
-  app.use(config.api.prefix, routes());
-};
+  app.use(config.api.prefix, routes())
+}
