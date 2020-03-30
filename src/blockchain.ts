cadcf67c2df003852098143e708a2c19686e460d
junha-ahn <junha.ahn.dev@gmail.com>
2020-03-30 16:47:35 +0900

    move test logic to mocha file

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -20,27 +20,27 @@ class Transaction {
 }
 
 export default class BlockChain {
-  static chain: Block[] = []
-  static pendingTransactions: Transaction[] = []
+  public chain: Block[] = []
+  public pendingTransactions: Transaction[] = []
   constructor() {}
-  static createNewBlcok = (nonce, previousBlockHash, hash) => {
+  public createNewBlcok = (nonce, previousBlockHash, hash) => {
     const newBlock = new Block(
-      BlockChain.chain.length + 1,
+      this.chain.length + 1,
       nonce,
-      BlockChain.pendingTransactions,
+      this.pendingTransactions,
       hash,
       previousBlockHash,
       Date.now()
     )
-    BlockChain.pendingTransactions = [];
-    BlockChain.chain.push(newBlock)
+    this.pendingTransactions = [];
+    this.chain.push(newBlock)
     return newBlock
   }
-  static getLastBlcok = (): Block => BlockChain.chain[BlockChain.chain.length - 1]
-  static createNewTransaction = (amount: number, sender: string, recipient: string): number => {
+  public getLastBlcok = (): Block => this.chain[this.chain.length - 1]
+  public createNewTransaction = (amount: number, sender: string, recipient: string): number => {
     const newTransaction = new Transaction(amount, sender, recipient)
-    BlockChain.pendingTransactions.push(newTransaction)
-    return BlockChain.getLastBlcok().index + 1
+    this.pendingTransactions.push(newTransaction)
+    return this.getLastBlcok().index + 1
   }
-  static hashBlock = (previousBlockHash, currentBlockData, nonce): string => sha256(previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData))
+  public hashBlock = (previousBlockHash, currentBlockData, nonce): string => sha256(previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData))
 }
--- test/blockchain.test.ts
+++ test/blockchain.test.ts
@@ -3,8 +3,42 @@ import BlockChainService from '../src/blockchain'
 const expect = chai.expect;
 
 
-describe('User', () => {
-  describe('/POST signup', () => {
-    console.log('Test Hello')
+describe('BlockChain', () => {
+  describe('createNewBlock 메소드 테스트', () => {
+    it ('생성 후, chain에 하나의 블럭이 있어야 한다', (done) => {
+      const bitcoin = new BlockChainService()
+      bitcoin.createNewBlcok(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf')
+      expect(bitcoin.chain.length).to.equal(1);
+      done()
+    }) 
+  })
+  describe('createNewTransaction 메소드 테스트', () => {
+    it ('생성 후, pendingTransactions에 하나의 기록이 있어야 한다', (done) => {
+      const bitcoin = new BlockChainService()
+      bitcoin.createNewBlcok(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf')
+      bitcoin.createNewTransaction(50, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56')
+      expect(bitcoin.chain[0].transactions.length).to.equal(0);
+      expect(bitcoin.pendingTransactions.length).to.equal(1);
+      done()
+    }) 
+    it ('미결 트랜잭션 테스트', (done) => {
+      // TODO: change test name. i cant find pertinent name
+      const bitcoin = new BlockChainService()
+      bitcoin.createNewBlcok(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf')
+      bitcoin.createNewTransaction(100, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56')
+      bitcoin.createNewBlcok(548764, 'OIUOEREDHKHKD', '78s97d4x6dsf')
+      
+      
+      bitcoin.createNewTransaction(50, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56')
+      bitcoin.createNewTransaction(200, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56')
+      bitcoin.createNewTransaction(300, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56')
+      
+      bitcoin.createNewBlcok(9878934, 'OIUOEREDHKHKD', '78s97d4x6dsf')
+      
+      expect(bitcoin.chain[0].transactions.length).to.equal(0);
+      expect(bitcoin.chain[2].transactions.length).to.equal(3);
+      expect(bitcoin.pendingTransactions.length).to.equal(0);
+      done()
+    }) 
   })
 });
