658dd157e50dce0e2c2248788296e500ec39997e
junha-ahn <junha.ahn.dev@gmail.com>
2020-03-30 15:27:37 +0900

    create class Block, BlockChain

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,1 +1,30 @@
-console.log('Hello World')
+class Block {
+  constructor(
+    public index:number,
+    public nonce: string,
+    public transactions: any,
+    public hash: string,
+    public previousHash: string,
+    public timestamp: number
+  ) {
+  }
+}
+
+export default class BlockChain {
+  static chain: Block[] = []
+  static newTransactions = []
+  constructor() {}
+  static createNewBlcok = (nonce, previousBlockHash, hash) => {
+    const newBlock = new Block(
+      BlockChain.chain.length + 1,
+      nonce,
+      BlockChain.newTransactions,
+      hash,
+      previousBlockHash,
+      Date.now()
+    )
+    // BlockChain.newTransactions = [];
+    BlockChain.chain.push(newBlock)
+    return newBlock
+  }
+}
